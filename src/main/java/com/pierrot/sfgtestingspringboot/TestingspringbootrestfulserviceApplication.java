cf8f96483de6d5ae8c9e20273e4130d147ee6b30
PierreSQS <nomo_messina@web.de>
2021-11-17 16:19:10 +0100

    Initial Setup

---
+++ src/main/java/com/pierrot/sfgtestingspringboot/TestingspringbootrestfulserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pierrot.sfgtestingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingspringbootrestfulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingspringbootrestfulserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pierrot/sfgtestingspringboot/TestingspringbootrestfulserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pierrot.sfgtestingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TestingspringbootrestfulserviceApplicationTests {
+
+	@Test
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
+	}
+
+}
